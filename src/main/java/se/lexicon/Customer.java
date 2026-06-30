c70d69de4bfcada415f6e60082d11b1af065a98f
Lina Abu Hijleh <lina_abuhijleh@hotmail.com>
2026-06-30 10:33:52 +0200

    product class

--- src/main/java/se/lexicon/Customer.java
+++ src/main/java/se/lexicon/Customer.java
@@ -1,4 +1,27 @@
 package se.lexicon;
 
 public class Customer {
+
+    private String name;
+    private String email;
+
+    Customer (String name, String email) {
+        if(name == null || name.isBlank() || email == null || email.isBlank()){
+            throw new IllegalArgumentException("Name and/or email cannot be null or blank");
+        }
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void printInfo() {
+        IO.println("Customer name is " + name + " and his email is " + email);
+    }
 }
--- src/main/java/se/lexicon/Product.java
+++ src/main/java/se/lexicon/Product.java
@@ -1,4 +1,37 @@
 package se.lexicon;
 
 public class Product {
+
+    private String name;
+    private double price;
+
+    Product(String name, double price) {
+        if(name == null || name.isBlank()){
+            throw new IllegalArgumentException("Product name cannot be null or blank");
+        }
+        if(price < 0){
+            throw new IllegalArgumentException("Product price cannot be negative");
+        }
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    private void applyDiscount(double percent) {
+        if(percent < 0 || percent > 80){
+            throw new IllegalArgumentException("Discount must be between 0 and 80%");
+        }
+        price -= (price * percent) / 100;
+    }
+
+    public void printInfo() {
+        IO.println("Product name is " + name + " and its current price is " + price + " SEK");
+    }
 }
